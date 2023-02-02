90aff8e0ba30ad845faf3b919a04732f75da6de0
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-02-02 18:06:44 +0800

    update

--- demo/src/main/java/com/luomor/controller/UploadController.java
+++ demo/src/main/java/com/luomor/controller/UploadController.java
@@ -1,7 +1,5 @@
-package com.neo.controller;
+package com.luomor.controller;
 
-import com.neo.fastdfs.FastDFSClient;
-import com.neo.fastdfs.FastDFSFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.luomor.fastdfs.FastDFSClient;
+import com.luomor.fastdfs.FastDFSFile;
 
 import java.io.IOException;
 import java.io.InputStream;
